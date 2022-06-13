f3d7be8dbfdd1d32a6a7c42831936d5dfef0039e
geroferrari <38739978+geroferrari@users.noreply.github.com>
2022-06-13 09:11:13 -0300

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -147,6 +147,6 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="gerooferrari@gmail.com"
-EMAIL_HOST_PASSWORD='rocoygero'
+EMAIL_HOST_PASSWORD=''
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
